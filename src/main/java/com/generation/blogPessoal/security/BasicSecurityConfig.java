c54cb3832aa21a044f3c782c08e7195d7fe28969
Weida Winy <winnyweida@hotmail.com>
2024-07-19 16:11:32 -0300

    Permitido que postagens sejam vistas sem token

--- src/main/java/com/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -64,6 +64,7 @@ public class BasicSecurityConfig {
 	        .authorizeHttpRequests((auth) -> auth
 	                .requestMatchers("/usuarios/logar").permitAll()
 	                .requestMatchers("/usuarios/cadastrar").permitAll()
+	                .requestMatchers("/postagens").permitAll()
 	                .requestMatchers("/error/**").permitAll()
 	                .requestMatchers(HttpMethod.OPTIONS).permitAll()
 	                .anyRequest().authenticated())
